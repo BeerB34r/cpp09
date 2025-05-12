c86c23cc847500a6da047d005658cb1b7f4abe71
Mats De Beer <mde-beer@student.codam.nl>
2025-05-12 19:30:06 +0200

    fixed it all up nicely

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,20 +1,31 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <fstream>
 
 int	main(int ac, char **av) {
-	try {
-		BitcoinExchange	wallStreet;
+	BitcoinExchange	wallStreet;
 
-		if (ac != 2) {
-			std::cerr << "USAGE: " << av[0] << " {YYYY-MM-DD} " << std::endl;
-			return 1;
-		}
-		try {
-			std::cout << "price of 1 btc at " << av[1] << " = " << wallStreet.getPrice(av[1], 1) << std::endl;
-		} catch (std::exception& err) {
-			std::cerr << err.what() << std::endl;
-		}
-	} catch (std::exception& err) {
-		std::cerr << err.what() << std::endl;
+	if (ac != 2) {
+		std::cerr << "USAGE: " << av[0] << " {input.txt}" << std::endl;
+		return 1;
 	}
+	std::ifstream	infile(av[1]);
+	if (infile.fail()) {
+		std::cerr << "Failed to open " << av[1] << std::endl;
+		return 2;
+	}
+	std::string	current;
+	do {
+		std::getline(infile, current);
+		if (current.empty() || current == "date | value") continue ;
+		try {
+			if (current.find('|') == std::string::npos) throw std::logic_error("malformed input => " + current);
+			std::string	timestring = current.substr(0, current.find('|') - 1);
+			std::string	balance = current.substr(current.find('|') + 1);
+			float	price = wallStreet.getPrice(timestring, std::stof(balance));
+			std::cout << timestring << "=>" << balance << " = " << price << std::endl;
+		}	catch(std::exception& err) {
+			std::cerr << "Error: " << err.what() << std::endl;
+		}
+	} while (!infile.eof());
 }
